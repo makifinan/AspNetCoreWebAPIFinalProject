759e40315ea3e30a38ef4da9eb9abd420890288f
mehmet akif inan <m.akifinan@outlook.com.tr>
2021-09-01 00:33:47 +0300

    Code Refactoring

---
+++ Business/Constants/Messages.cs
@@ -0,0 +1,17 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Constants
+{
+    public static class Messages
+    {
+        //static yapılar
+        //base message
+        public static string ProductAdded = "Ürün Eklendi";
+        public static string ProductNameInvalid = "Ürün İsmi Geçersiz";
+        public static string MaintenanceTime="Sistem Bakımda";
+        public static string ProductsListed="Ürünler listelendi";
+    }
+}
